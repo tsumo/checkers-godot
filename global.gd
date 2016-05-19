af85bce40d101eb34cf550c57b8f74378b6e6eaa
tsumo <tsumo@users.noreply.github.com>
2016-05-19 22:42:38 +0300

    trying to move pieces to global state

--- global.gd
+++ global.gd
@@ -7,7 +7,7 @@ extends Node
 var white_pieces = []
 var black_pieces = []
 
-var piece = preload("res://white_piece.scn").instance()
+var piece = preload("res://piece.scn").instance()
 
 var selected_piece_pos
 var selected_piece_name
@@ -15,7 +15,7 @@ var selected_piece_name
 func _ready():
 	for i in range(12):
 		pass
-	get_node(".").add_child(piece)
+	get_node("res://main.scn").add_child(piece)
 	#white_pieces[0].pos = Vector2(30, 30)
 	
 
