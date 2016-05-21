48b3ac92ecc0608ba73300264f736c1fd860eae7
tsumo <tsumo@users.noreply.github.com>
2016-05-21 18:14:32 +0300

    store global board status in 2d array

--- board_logic.gd
+++ board_logic.gd
@@ -20,6 +20,7 @@ func _input(event):
 	if event.type == InputEvent.MOUSE_BUTTON \
 	and event.button_index == BUTTON_RIGHT \
 	and event.pressed:
+		print_state()
 		if global.selected_piece_name != "None":
 			global.selected_piece_name = "None"
 			global.selected_piece_pos = "None"
@@ -35,10 +36,11 @@ func _ready():
 	# Instance and position black pieces
 	for i in range(12):
 		var piece = piece_scn.instance()
-		piece.color = "black"
+		piece.color = "b"
 		var x = [1, 3, 5, 7, 0, 2, 4, 6, 1, 3, 5, 7]
 		var y = i / 4
 		piece.set_pos(board_nd.map_to_world(Vector2(x[i], y)) + Vector2(32, 32))
+		global.state[y][x[i]] = "b"
 		var sprite_nd = piece.get_node("sprite")
 		sprite_nd.set_texture(black_piece_txtr)
 		self.add_child(piece)
@@ -46,10 +48,11 @@ func _ready():
 	# # Instance and position white pieces
 	for i in range(12):
 		var piece = piece_scn.instance()
-		piece.color = "white"
+		piece.color = "w"
 		var x = [0, 2, 4, 6, 1, 3, 5, 7, 0, 2, 4, 6]
 		var y = (i / 4) + 5
 		piece.set_pos(board_nd.map_to_world(Vector2(x[i], y)) + Vector2(32, 32))
+		global.state[y][x[i]] = "w"
 		var sprite_nd = piece.get_node("sprite")
 		sprite_nd.set_texture(white_piece_txtr)
 		self.add_child(piece)
@@ -59,3 +62,9 @@ func _process(delta):
 	label_nd.set_text(str("selected: ", global.selected_piece_name))
 	label_nd.set_text(label_nd.get_text() + str(" at: ", global.selected_piece_pos))
 	label_nd.set_text(label_nd.get_text() + str(" color: ", global.selected_piece_color))
+
+
+func print_state():
+	print("State:")
+	for i in global.state:
+		print(i)
--- global.gd
+++ global.gd
@@ -7,3 +7,16 @@ extends Node
 var selected_piece_name = "None"
 var selected_piece_pos = "None"
 var selected_piece_color = "None"
+
+# Stores positions of pieces
+var state = []
+
+
+func _ready():
+	# Dash for empty square
+	# w for white piece
+	# b for black piece
+	for i in range(8):
+		state.append([])
+		for j in range(8):
+			state[i].append("-")
