324de77b961511961d510f87769fdefb62268caf
tsumo <tsumo@users.noreply.github.com>
2016-05-22 14:35:50 +0300

    little refactoring

--- board_logic.gd
+++ board_logic.gd
@@ -29,9 +29,7 @@ func _input(event):
 	and event.button_index == BUTTON_LEFT \
 	and event.pressed:
 		if global.selected_piece_name != "None":
-			print(board_nd.world_to_map(event.pos))
-			#print(board_nd.world_to_map(board_nd.map_to_world(event.pos)))
-			get_node(global.selected_piece_name).set_pos(board_nd.map_to_world(board_nd.world_to_map(event.pos)) + Vector2(32, 32))
+			move_selected_to(event.pos)
 			deselect_piece()
 			# Stop event from propagating further
 			self.get_tree().set_input_as_handled()
@@ -43,19 +41,18 @@ func _ready():
 	
 	randomize()
 	
-	# Instance and position black pieces
-	for i in range(12):
-		var piece = piece_scn.instance()
-		piece.color = "b"
-		var x = [1, 3, 5, 7, 0, 2, 4, 6, 1, 3, 5, 7]
-		var y = i / 4
-		piece.set_pos(board_nd.map_to_world(Vector2(x[i], y)) + Vector2(32, 32))
-		global.state[x[i]][y] = "b"
-		var sprite_nd = piece.get_node("sprite")
-		sprite_nd.set_texture(black_piece_txtr)
-		self.add_child(piece)
-	
-	# Instance and position white pieces
+	init_black()
+	init_white()
+
+
+func _process(delta):
+	label_nd.set_text(str("selected: ", global.selected_piece_name))
+	label_nd.set_text(label_nd.get_text() + str(" at: ", global.selected_piece_pos))
+	label_nd.set_text(label_nd.get_text() + str(" color: ", global.selected_piece_color))
+
+
+# Instance and position white pieces
+func init_white():
 	for i in range(12):
 		var piece = piece_scn.instance()
 		piece.color = "w"
@@ -68,16 +65,38 @@ func _ready():
 		self.add_child(piece)
 
 
-func _process(delta):
-	label_nd.set_text(str("selected: ", global.selected_piece_name))
-	label_nd.set_text(label_nd.get_text() + str(" at: ", global.selected_piece_pos))
-	label_nd.set_text(label_nd.get_text() + str(" color: ", global.selected_piece_color))
+# Instance and position black pieces
+func init_black():
+	for i in range(12):
+		var piece = piece_scn.instance()
+		piece.color = "b"
+		var x = [1, 3, 5, 7, 0, 2, 4, 6, 1, 3, 5, 7]
+		var y = i / 4
+		piece.set_pos(board_nd.map_to_world(Vector2(x[i], y)) + Vector2(32, 32))
+		global.state[x[i]][y] = "b"
+		var sprite_nd = piece.get_node("sprite")
+		sprite_nd.set_texture(black_piece_txtr)
+		self.add_child(piece)
 
 
 func deselect_piece():
 	global.selected_piece_name = "None"
 	global.selected_piece_pos = "None"
 	global.selected_piece_color = "None"
+
+
+func move_selected_to(pos):
+	# Transform global click pos to board coordinates and back
+	# to move piece exactly to the clicked square.
+	# Add offset to position it in center of the square
+	get_node(global.selected_piece_name).set_pos(board_nd.map_to_world(board_nd.world_to_map(pos)) + Vector2(32, 32))
+	# Update global state
+	var x_from = global.selected_piece_pos.x
+	var y_from = global.selected_piece_pos.y
+	var x_to = board_nd.world_to_map(pos).x
+	var y_to = board_nd.world_to_map(pos).y
+	global.state[x_from][y_from] = "-"
+	global.state[x_to][y_to] = global.selected_piece_color
 
 
 func print_board_state():
